13cca63e44f79a4165414a3226f617818d01c18b
Rashad Edwards <rashadedwards@macbook.local>
2024-10-05 20:50:11 -0400

    game update

--- JavaScript/gameAnimations.js
+++ JavaScript/gameAnimations.js
@@ -111,14 +111,14 @@ function ChefImage() {
     },
     { passive: false }
   );
-}
 
-document.addEventListener("gesturestart", function (e) {
-  e.preventDefault();
-});
-document.addEventListener("gesturechange", function (e) {
-  e.preventDefault();
-});
-document.addEventListener("gestureend", function (e) {
-  e.preventDefault();
-});
+  document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+  });
+  document.addEventListener("gesturechange", function (e) {
+    e.preventDefault();
+  });
+  document.addEventListener("gestureend", function (e) {
+    e.preventDefault();
+  });
+}
