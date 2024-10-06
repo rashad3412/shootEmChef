263a6345272e245295413f1af1f4f7496f1a68ca
Rashad Edwards <rashadedwards@macbook.local>
2024-10-06 19:42:46 -0400

    updated music

--- JavaScript/backgroundMusic.js
+++ JavaScript/backgroundMusic.js
@@ -1,6 +1,8 @@
 let backgroundMusic = new Audio("sounds/backgroundMusic.mp3");
 backgroundMusic.loop = true;
 backgroundMusic.volume = 0.3;
+const startTime = 20;
+backgroundMusic.currentTime = startTime;
 // backgroundMusic.playbackRate = 0.9;
 
 function playBackgroundMusic() {
