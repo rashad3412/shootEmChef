c8cf879f0213a72f46d912c6fae3663264fb4025
Rashad Edwards <rashadedwards@macbook.local>
2024-10-07 20:03:45 -0400

    updated commit

--- JavaScript/gameControls.js
+++ JavaScript/gameControls.js
@@ -4,14 +4,6 @@ Game Controls
 
 */
 
-//  checking to see if the session for game.html has beem closed or refreshed on the page if the page is refreshed the window will redirect to the index.html file.
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   if (!sessionStorage.getItem("navigated")) {
-//     window.location.href = "index.html";
-//   }
-//   sessionStorage.removeItem("navigated");
-// });
 // Game Functions
 
 function shootImages(fromElement) {
